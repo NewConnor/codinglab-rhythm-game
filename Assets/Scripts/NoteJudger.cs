46f9528456cb7a2f5baff6e85f7bfc98c8c4e260
NewConnor <connorpriet@gmail.com>
2025-10-27 20:10:25 +0900

    Update NoteJudger.cs

--- Assets/Scripts/NoteJudger.cs
+++ Assets/Scripts/NoteJudger.cs
@@ -4,6 +4,9 @@ public class NoteJudger : MonoBehaviour
 {
     public int lineIndex;
     public float targetY = -3f;
+
+    // 아래 pefectRange와 goodRange는 실제 판정 결과에 사용되는 상수입니다.
+    // 수정이 필요할 시, 인스펙터에서 수정하는 것을 권장합니다.
     public float perfectRange = 0.3f;
     public float goodRange = 0.7f;
 
